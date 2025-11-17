f46c0d520d550f3d9573a6642d725d6d76181100
Rahul <talwarrahul295@gmail.com>
2025-11-18 01:23:03 +0530

    testingg

--- mock/src/components/HooksTest.jsx
+++ mock/src/components/HooksTest.jsx
@@ -1,20 +1,36 @@
 /* eslint-disable no-unused-vars */
 // import React from 'react'
 
-import { useMemo, useReducer } from "react"
+import { useEffect, useMemo, useReducer, useState } from "react"
 
 function reducer(state,action){
-    switch (action.type) {
-        case "increment" : return {count : state.count + 1};
-        case "decrement" : return {count : state.count - 1};
-        case "reset" : return {count : 0};
-        default : return state;
-    }
+  switch(action.type) {
+    case  "increment" :
+      return {count: state.count + 1};
+    case "decrement" :
+      return {count: state.count - 1};
+    case "reset" :
+      return {count : 0};
+    default : 
+      return state;
+  }
 }
 
-function HooksTest() {
 
-    const [state,dispatch] = useReducer(reducer, {count : 0});
+
+function HooksTest() {
+  
+  useEffect(() => {
+  console.log("hey mounting");
+  },[])
+
+  const [value,setValue] = useState("");
+  const [state,dispatch] = useReducer(reducer,{count : 0});
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(value);
+  }
 
     // use memo
     const [numbers] = [1,2,3,4,5,6,7,8,9,10];
@@ -31,6 +47,11 @@ function HooksTest() {
       {even.map((e) => (
         <li key={e}>e</li>
       ))}
+      <form action="">
+
+      <input type="text" placeholder="heyyy" value={value} onChange={(e) => setValue(e.target.value)} />
+      <button type="submit" onClick={handleSubmit}>hey</button>
+      </form>
     </div>
   )
 }
